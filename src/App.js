c606428ab87a7a0c8b34d06102fdf3a2094bd2e5
Dejan <dejan.belic@develomon.com>
2019-04-08 21:11:32 +0200

    Cleaning up by returning a function from the effect.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
 function App() {
-  const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
-  const handleCityChange = event => setCity(event.target.value);
-  const handleCountryChange = event => setCountry(event.target.value);
+  const [count, setCount] = useState(0);
+  const handleIncreaseCount = () => setCount(count + 1);
+  const handleDecreaseCount = () => setCount(count - 1);
 
 
+  useEffect(() => {
+    console.log(`I'm inside the useEffect function. The current count is ${count}`);
+
+    return () => {
+      console.log(`Queed to clean up above work after its executed. ${count}`)
+    }
+  });
 
   return (
-    <form>
-      <div>
-        <input
-          type="text"
-          placeholder="City"
-          value={city}
-          onChange={handleCityChange}
-        />
-      </div>
-
-      <div>
-        <input
-          type="text"
-          placeholder="Country"
-          value={country}
-          onChange={handleCountryChange}
-        />
-      </div>
-
-      <div>You live in {`${city}, ${country}`}</div>
-    </form>
+    <div>
+      <button onClick={handleIncreaseCount}>Increase</button>
+      <button onClick={handleDecreaseCount}>Decrease</button>
+      <h1>count</h1>
+    </div>
   )
 }
 

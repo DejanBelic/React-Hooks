4f47d04894c0101c316bf9545b4488f63eb169a7
Dejan <dejan.belic@develomon.com>
2019-04-09 19:33:38 +0200

    useEffect hook user input example.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState('salmon');
-  const handleIncreaseCount = () => setCount(count + 1);
-  const handleDecreaseCount = () => setCount(count - 1);
+function App() {
+  const [userText, setUserText] = useState('');
 
+  const handleUserKeyPress = event => {
+    const { key, keyCode } = event;
+    if (keyCode === 32 || (keyCode >= 65 && keyCode <= 90)) {
+      setUserText(`${userText}${key}`)
+    }
+  };
 
   useEffect(() => {
-    console.log(`I'm inside the useEffect function. The current count is ${count}`);
+    window.addEventListener('keydown', handleUserKeyPress);
 
     return () => {
-      console.log(`Queed to clean up above work after its executed. ${count}`)
+      window.removeEventListener('keydown', handleUserKeyPress)
     }
-  }, [count]);
-
-  const handleColorChange = () =>  {
-    const nextColor = color === 'salmon' ? 'gold' : 'salmon';
-    setColor(nextColor);
-  };
+  });
 
   return (
     <div>
-      <button onClick={handleIncreaseCount}>Increase</button>
-      <button onClick={handleColorChange}>Color change</button>
-      <button onClick={handleDecreaseCount}>Decrease</button>
-      <h1 style={{ color }}>count</h1>
-    </div>
-  )
-}
-
-function App() {
-  const [visible, setVisible] = useState(false);
-  return(<div>
-    <button onClick={() => setVisible(!visible)}>Show / Hide the Counter Component</button>
-    { visible &&  <Counter/>}
-  </div>)
+      <h1>Feel free to type</h1>
+      <blockquote>
+        { userText }
+      </blockquote>
+    </div>)
 }
 
 export default App;
